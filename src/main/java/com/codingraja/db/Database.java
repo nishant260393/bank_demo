8ce5deda4426b8b4ad8f4f762c4844de39842dbc
nishant260393 <nishant260393@gmail.com>
2020-07-14 19:31:03 +0000

    first commit

---
+++ src/main/java/com/codingraja/db/Database.java
@@ -0,0 +1,38 @@
+package com.codingraja.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Database {
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://172.31.43.124:3306/demo";
+	private static final String USERNAME = "inguser1";
+	private static final String PASSWORD = "H@ckath0n";
+	
+	private static Connection connection = null;
+	
+	public static Connection getConnection(){
+		if(connection==null){
+			try{
+				//Loading The Driver Class
+				Class.forName(DRIVER);
+				
+				//Getting the connection Object
+				connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			}catch (Exception ex) {
+				System.out.println(ex.getMessage());
+			}
+		}
+		
+		return connection;
+	}
+}
+
+/*CREATE TABLE customer_master(
+	customer_id BIGINT UNIQUE NOT NULL AUTO_INCREMENT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50) UNIQUE NOT NULL,
+	mobile VARCHAR(50),
+	PRIMARY KEY(customer_id)
+);*/
